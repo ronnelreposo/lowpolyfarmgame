182b1c982d0ed634df7fa972f70ad3cda896ee50
ronnelreposo <ronnel.reposo@gmail.com>
2025-11-01 23:18:08 +0800

    Format meshes typescript

--- src/app/models/puppy.ts
+++ src/app/models/puppy.ts
@@ -6,47 +6,128 @@ const headGeom: Tree<Model> = createNode<Model>(
 	{
 		id: "head-base",
 		mesh: unitCube("unit-cube"),
-		trs: { t: [0, 0.9, 0], pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.7 },
+		trs: {
+			t: [0, 0.9, 0],
+			pivot: [0, 0, 0],
+			rxdeg: 0,
+			rydeg: 0,
+			rzdeg: 0,
+			s: 0.7,
+		},
 		// To be filled by update world.
 		modelMatrix: [],
-		material: { basecolor: [] }
+		material: { basecolor: [] },
 	},
 	[
 		createLeaf({
 			id: "left-ear",
 			mesh: unitCube("unit-cube"),
-			trs: { t: [1.0, 1.0, 0], pivot: [-0.5, -0.5, 0], rxdeg: 0, rydeg: 0, rzdeg: -25, s: 0.5 },
+			trs: {
+				t: [1.0, 1.0, 0],
+				pivot: [-0.5, -0.5, 0],
+				rxdeg: 0,
+				rydeg: 0,
+				rzdeg: -25,
+				s: 0.5,
+			},
 			// To be filled by update world.
 			modelMatrix: [],
-			material: { basecolor: [] }
+			material: { basecolor: [] },
 		}),
 		createLeaf({
 			id: "head-base",
 			mesh: unitCube("unit-cube"),
-			trs: { id: "right-ear", t: [-1.0, 1.0, 0], pivot: [0.5, -0.5, 0], rxdeg: 0, rydeg: 0, rzdeg: -25, s: 0.5 },
+			trs: {
+				id: "right-ear",
+				t: [-1.0, 1.0, 0],
+				pivot: [0.5, -0.5, 0],
+				rxdeg: 0,
+				rydeg: 0,
+				rzdeg: -25,
+				s: 0.5,
+			},
 			// To be filled by update world.
 			modelMatrix: [],
-			material: { basecolor: [] }
-		})
-	]
+			material: { basecolor: [] },
+		}),
+	],
 );
-
 
 function createCuberMan(id: string) {
 	return createNode<Model>(
 		{
 			id: `cuberman:${id}`,
 			mesh: unitCube("unit-cube"),
-			trs: { t: [0, 0, 0], pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.5 },
-			modelMatrix: [], material: { basecolor: [] }
+			trs: {
+				t: [0, 0, 0],
+				pivot: [0, 0, 0],
+				rxdeg: 0,
+				rydeg: 0,
+				rzdeg: 0,
+				s: 0.5,
+			},
+			modelMatrix: [],
+			material: { basecolor: [] },
 		},
 		[
 			headGeom,
-			createLeaf({ id: "left-arm", mesh: unitCube("unit-cube"), modelMatrix: [], material: { basecolor: [] }, trs: { t: [0.8, -0.3, 0], pivot: [0.0, 0.5, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.5 } }),
-			createLeaf({ id: "right-arm", mesh: unitCube("unit-cube"), modelMatrix: [], material: { basecolor: [] }, trs: { t: [-0.8, -0.3, 0], pivot: [0.0, 0.5, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.5 } }),
-			createLeaf({ id: "left-leg", mesh: unitCube("unit-cube"), modelMatrix: [], material: { basecolor: [] }, trs: { t: [0.3, -1.1, 0], pivot: [0.0, 0.5, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.5 } }),
-			createLeaf({ id: "right-leg", mesh: unitCube("unit-cube"), modelMatrix: [], material: { basecolor: [] }, trs: { t: [-0.3, -1.1, 0], pivot: [0.0, 0.5, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.5 } }),
-		]
+			createLeaf({
+				id: "left-arm",
+				mesh: unitCube("unit-cube"),
+				modelMatrix: [],
+				material: { basecolor: [] },
+				trs: {
+					t: [0.8, -0.3, 0],
+					pivot: [0.0, 0.5, 0],
+					rxdeg: 0,
+					rydeg: 0,
+					rzdeg: 0,
+					s: 0.5,
+				},
+			}),
+			createLeaf({
+				id: "right-arm",
+				mesh: unitCube("unit-cube"),
+				modelMatrix: [],
+				material: { basecolor: [] },
+				trs: {
+					t: [-0.8, -0.3, 0],
+					pivot: [0.0, 0.5, 0],
+					rxdeg: 0,
+					rydeg: 0,
+					rzdeg: 0,
+					s: 0.5,
+				},
+			}),
+			createLeaf({
+				id: "left-leg",
+				mesh: unitCube("unit-cube"),
+				modelMatrix: [],
+				material: { basecolor: [] },
+				trs: {
+					t: [0.3, -1.1, 0],
+					pivot: [0.0, 0.5, 0],
+					rxdeg: 0,
+					rydeg: 0,
+					rzdeg: 0,
+					s: 0.5,
+				},
+			}),
+			createLeaf({
+				id: "right-leg",
+				mesh: unitCube("unit-cube"),
+				modelMatrix: [],
+				material: { basecolor: [] },
+				trs: {
+					t: [-0.3, -1.1, 0],
+					pivot: [0.0, 0.5, 0],
+					rxdeg: 0,
+					rydeg: 0,
+					rzdeg: 0,
+					s: 0.5,
+				},
+			}),
+		],
 	);
 }
 
@@ -61,12 +142,16 @@ function terrain(row: number, col: number, gap = 0.01): Tree<Model> {
 					t: [
 						(i - row / 2) * (1 + gap),
 						0,
-						(j - row / 2) * (1 + gap)
+						(j - row / 2) * (1 + gap),
 					],
 					pivot: [0, 0, 0],
-					rxdeg: 0, rydeg: 0, rzdeg: 0, s: 1
+					rxdeg: 0,
+					rydeg: 0,
+					rzdeg: 0,
+					s: 1,
 				},
-				modelMatrix: [], material: { basecolor: [] }
+				modelMatrix: [],
+				material: { basecolor: [] },
 			});
 		}
 	}
@@ -74,11 +159,19 @@ function terrain(row: number, col: number, gap = 0.01): Tree<Model> {
 		{
 			id: `root-anchor`,
 			mesh: unitCube("unit-cube"),
-			trs: { t: [0, 0, 0], pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 1.0 },
-			modelMatrix: [], material: { basecolor: [] }
+			trs: {
+				t: [0, 0, 0],
+				pivot: [0, 0, 0],
+				rxdeg: 0,
+				rydeg: 0,
+				rzdeg: 0,
+				s: 1.0,
+			},
+			modelMatrix: [],
+			material: { basecolor: [] },
 		},
-		spanModels.map(createLeaf)
-	)
+		spanModels.map(createLeaf),
+	);
 }
 
 export const terrainWidth = 0;
@@ -90,71 +183,132 @@ export const cuberManCubeCount = 8;
 const carrotOffset = 1.0;
 const carrotBodyHeight = 0.5 + 0.7 + 0.4 + carrotOffset;
 const carrotStalkAndLeavesColors = [
-	...Array(6).fill(rgbaToColor(39, 174, 96)).flat(),
-	...Array(6).fill(rgbaToColor(39, 174, 96)).flat(),
-	...Array(6).fill(rgbaToColor(39, 174, 96)).flat(),
-	...Array(6).fill(rgbaToColor(39, 174, 96)).flat(),
-	...Array(6).fill(rgbaToColor(46, 204, 113)).flat(), // emerald
-	...Array(6).fill(rgbaToColor(39, 174, 96)).flat(), // nephritis
-]
+	...Array(6)
+		.fill(rgbaToColor(39, 174, 96))
+		.flat(),
+	...Array(6)
+		.fill(rgbaToColor(39, 174, 96))
+		.flat(),
+	...Array(6)
+		.fill(rgbaToColor(39, 174, 96))
+		.flat(),
+	...Array(6)
+		.fill(rgbaToColor(39, 174, 96))
+		.flat(),
+	...Array(6)
+		.fill(rgbaToColor(46, 204, 113))
+		.flat(), // emerald
+	...Array(6)
+		.fill(rgbaToColor(39, 174, 96))
+		.flat(), // nephritis
+];
 const carrotBodyColors = [
-	...Array(6).fill(rgbaToColor(230, 126, 34)).flat(), // orange
-	...Array(6).fill(rgbaToColor(230, 126, 34)).flat(),
-	...Array(6).fill(rgbaToColor(230, 126, 34)).flat(),
-	...Array(6).fill(rgbaToColor(230, 126, 34)).flat(),
-	...Array(6).fill(rgbaToColor(243, 156, 18)).flat(), // carrot
-	...Array(6).fill(rgbaToColor(230, 126, 34)).flat(),
+	...Array(6)
+		.fill(rgbaToColor(230, 126, 34))
+		.flat(), // orange
+	...Array(6)
+		.fill(rgbaToColor(230, 126, 34))
+		.flat(),
+	...Array(6)
+		.fill(rgbaToColor(230, 126, 34))
+		.flat(),
+	...Array(6)
+		.fill(rgbaToColor(230, 126, 34))
+		.flat(),
+	...Array(6)
+		.fill(rgbaToColor(243, 156, 18))
+		.flat(), // carrot
+	...Array(6)
+		.fill(rgbaToColor(230, 126, 34))
+		.flat(),
 ];
 // Bare no colors and not placed in the world yet.
 export const myModelWorld: Tree<Model> = createNode<Model>(
 	{
 		id: "root-anchor",
 		mesh: unitCube("unit-cube"),
-		trs: { t: [0, 0, 0], pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 1.0, },
+		trs: {
+			t: [0, 0, 0],
+			pivot: [0, 0, 0],
+			rxdeg: 0,
+			rydeg: 0,
+			rzdeg: 0,
+			s: 1.0,
+		},
 		// To be filled by update world.
 		modelMatrix: [],
-		material: { basecolor: [] }
+		material: { basecolor: [] },
 	},
 	[
 		// Cuberman army!
-		...Array(cuberManCount).fill(null).map((x, i) => createCuberMan(i.toString())),
+		...Array(cuberManCount)
+			.fill(null)
+			.map((x, i) => createCuberMan(i.toString())),
 		// Terrain.
 		terrain(terrainWidth, terrainHeight),
 
-
 		// carrot.
-		createNode({
-			id: "root-anchor",
-			mesh: unitCube("unit-cube"),
-			trs: { t: [2, -2, 0], pivot: [0, 1, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.5, },
-			// To be filled by update world.
-			modelMatrix: [],
-			material: { basecolor: [] }
-		},
+		createNode(
+			{
+				id: "root-anchor",
+				mesh: unitCube("unit-cube"),
+				trs: {
+					t: [0, 2, 0],
+					pivot: [0, 1, 0],
+					rxdeg: 0,
+					rydeg: 0,
+					rzdeg: 0,
+					s: 0.5,
+				},
+				// To be filled by update world.
+				modelMatrix: [],
+				material: { basecolor: [] },
+			},
 			[
 				createLeaf({
 					id: "carrot-body1",
 					mesh: unitCube("unit-cube"),
-					trs: { t: [0, 0.2 + carrotOffset, 0], pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.4, }, // 0.4
+					trs: {
+						t: [0, 0.2 + carrotOffset, 0],
+						pivot: [0, 0, 0],
+						rxdeg: 0,
+						rydeg: 0,
+						rzdeg: 0,
+						s: 0.4,
+					}, // 0.4
 					// To be filled by update world.
 					modelMatrix: [],
-					material: { basecolor: carrotBodyColors }
+					material: { basecolor: carrotBodyColors },
 				}),
 				createLeaf({
 					id: "carrot-body2",
 					mesh: unitCube("unit-cube"),
-					trs: { t: [0, 0.35 + 0.4 + carrotOffset, 0], pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.7, }, // 0.7
+					trs: {
+						t: [0, 0.35 + 0.4 + carrotOffset, 0],
+						pivot: [0, 0, 0],
+						rxdeg: 0,
+						rydeg: 0,
+						rzdeg: 0,
+						s: 0.7,
+					}, // 0.7
 					// To be filled by update world.
 					modelMatrix: [],
-					material: { basecolor: carrotBodyColors }
+					material: { basecolor: carrotBodyColors },
 				}),
 				createLeaf({
 					id: "carrot-body3",
 					mesh: unitCube("unit-cube"),
-					trs: { t: [0, carrotBodyHeight, 0], pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 1.0, }, // 1.
+					trs: {
+						t: [0, carrotBodyHeight, 0],
+						pivot: [0, 0, 0],
+						rxdeg: 0,
+						rydeg: 0,
+						rzdeg: 0,
+						s: 1.0,
+					}, // 1.
 					// To be filled by update world.
 					modelMatrix: [],
-					material: { basecolor: carrotBodyColors }
+					material: { basecolor: carrotBodyColors },
 				}),
 				// Carrot leaves.
 				createLeaf({
@@ -163,13 +317,19 @@ export const myModelWorld: Tree<Model> = createNode<Model>(
 					trs: {
 						t: [
 							0,
-							0.7 // (size/2) + (prev cube size / 2).
-							+ carrotBodyHeight, 0
-						], pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.4,
+							0.7 + // (size/2) + (prev cube size / 2).
+								carrotBodyHeight,
+							0,
+						],
+						pivot: [0, 0, 0],
+						rxdeg: 0,
+						rydeg: 0,
+						rzdeg: 0,
+						s: 0.4,
 					},
 					// To be filled by update world.
 					modelMatrix: [],
-					material: { basecolor: carrotStalkAndLeavesColors }
+					material: { basecolor: carrotStalkAndLeavesColors },
 				}),
 				createLeaf({
 					id: "carrot-leaf1",
@@ -178,13 +338,17 @@ export const myModelWorld: Tree<Model> = createNode<Model>(
 						t: [
 							0,
 							0.2 + 0.7 + carrotBodyHeight, // (size/2) + (prev cube size / 2).
-							0.4 // offset a bit.
+							0.4, // offset a bit.
 						],
-						pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.4,
+						pivot: [0, 0, 0],
+						rxdeg: 0,
+						rydeg: 0,
+						rzdeg: 0,
+						s: 0.4,
 					},
 					// To be filled by update world.
 					modelMatrix: [],
-					material: { basecolor: carrotStalkAndLeavesColors }
+					material: { basecolor: carrotStalkAndLeavesColors },
 				}),
 				createLeaf({
 					id: "carrot-leaf2",
@@ -193,13 +357,17 @@ export const myModelWorld: Tree<Model> = createNode<Model>(
 						t: [
 							0.4, // offset a bit.
 							0.2 + 0.7 + carrotBodyHeight, // (size/2) + (leaf base size / 2).
-							0
+							0,
 						],
-						pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.4,
+						pivot: [0, 0, 0],
+						rxdeg: 0,
+						rydeg: 0,
+						rzdeg: 0,
+						s: 0.4,
 					},
 					// To be filled by update world.
 					modelMatrix: [],
-					material: { basecolor: carrotStalkAndLeavesColors }
+					material: { basecolor: carrotStalkAndLeavesColors },
 				}),
 				createLeaf({
 					id: "carrot-leaf3",
@@ -208,13 +376,17 @@ export const myModelWorld: Tree<Model> = createNode<Model>(
 						t: [
 							-0.4, // offset a bit.
 							0.2 + 0.7 + carrotBodyHeight, // (size/2) + (leaf base size / 2).
-							0
+							0,
 						],
-						pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.4,
+						pivot: [0, 0, 0],
+						rxdeg: 0,
+						rydeg: 0,
+						rzdeg: 0,
+						s: 0.4,
 					},
 					// To be filled by update world.
 					modelMatrix: [],
-					material: { basecolor: carrotStalkAndLeavesColors }
+					material: { basecolor: carrotStalkAndLeavesColors },
 				}),
 				createLeaf({
 					id: "carrot-leaf4",
@@ -223,15 +395,19 @@ export const myModelWorld: Tree<Model> = createNode<Model>(
 						t: [
 							0,
 							0.2 + 0.7 + carrotBodyHeight, // (size/2) + (leaf base size / 2).
-							-0.4 // offset a bit.
+							-0.4, // offset a bit.
 						],
-						pivot: [0, 0, 0], rxdeg: 0, rydeg: 0, rzdeg: 0, s: 0.4,
+						pivot: [0, 0, 0],
+						rxdeg: 0,
+						rydeg: 0,
+						rzdeg: 0,
+						s: 0.4,
 					},
 					// To be filled by update world.
 					modelMatrix: [],
-					material: { basecolor: carrotStalkAndLeavesColors }
-				})
-			]
+					material: { basecolor: carrotStalkAndLeavesColors },
+				}),
+			],
 		),
 	],
 );
