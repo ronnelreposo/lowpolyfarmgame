d6c94ac617cb8274d2bb241c4a5b8aa95ff121b9
Ronnel Reposo <ronnel.reposo@gmail.com>
2025-11-30 21:15:21 +0800

    Cleanup

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -13,36 +13,4 @@ describe("App", () => {
 		const app = fixture.componentInstance;
 		expect(app).toBeTruthy();
 	});
-
-	// // example dom.
-	// it("should render title", () => {
-	// 	const fixture = TestBed.createComponent(App);
-	// 	fixture.detectChanges();
-	// 	const compiled = fixture.nativeElement as HTMLElement;
-	// 	expect(compiled.querySelector("h1")?.textContent).toContain(
-	// 		"Hello, angular20-playground",
-	// 	);
-	// });
-
-	it("should compute.", () => {
-		const fixture = TestBed.createComponent(App);
-		const app = fixture.componentInstance;
-		// initial answer.
-		expect(app.answer).toEqual(0);
-		// try to compute.
-		app.compute();
-		expect(app.answer).toEqual(1);
-	});
-
-	it("should compute and bind.", () => {
-		const fixture = TestBed.createComponent(App);
-		const app = fixture.componentInstance;
-		// initial answer.
-		expect(app.answer).toEqual(0);
-		// try to compute.
-		app.compute();
-		fixture.detectChanges();
-		const compiled = fixture.nativeElement as HTMLElement;
-		expect(+(compiled.querySelector('#answer')?.textContent ?? 0)).toEqual(1);
-	});
 });
